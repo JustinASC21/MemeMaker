28bc75a417b7d3ed9e21bebc014cc8c54f17beab
JustinASC21 <justinlema0012@gmail.com>
2021-08-31 21:26:50 -0400

    Second Commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // our meme container
 let memeContainer = document.getElementById("memeContainer");
 let meme = document.getElementById("meme");
+let newMemeButton = document.getElementById("newMeme"); // our meme button
+let boxContainer = document.getElementById("boxInputs");
 
 
 // previous functions defined here
@@ -8,6 +10,7 @@ function random(min,max) {
     let randInt = Math.floor(Math.random() * (max - min)) + min;
     return randInt;
 }
+
 
 // grab api 
 url = "https://api.imgflip.com/get_memes"
@@ -17,9 +20,42 @@ fetch(url)
         return response.json();
     })
     .then(function(json) {
-        console.log(json)
+
         randomObj = json["data"]["memes"][random(0,100)]
         randomImg = randomObj.url;
+        randomBoxCounts = randomObj.box_count // important to display to the user how many boxes to fill in
+
+        for (let box = 0; box < randomBoxCounts; box ++) {
+            // create box inputs for each of the text boxes to caption
+            let inputBox = document.createElement("input");
+            inputBox.setAttribute("type","text");
+
+            // add inputs to box container
+            boxContainer.appendChild(inputBox);
+
+        }
+    
         meme.src = randomImg;
-        console.log(randomObj.box_count)
+        console.log(randomBoxCounts)
+
+        newMemeButton.onclick = function(event) { // when newMeme button is clicked
+            event.preventDefault();
+
+            url = "https://api.imgflip.com/get_memes"
+        // this api returns about 100 meme templates
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+
+                randomObj = json["data"]["memes"][random(0,100)]
+                randomImg = randomObj.url;
+                randomBoxCounts = randomObj.box_count // important to display to the user how many boxes to fill in
+            
+            
+                meme.src = randomImg;
+                console.log(randomBoxCounts)
+                })
+        }
     })
